35c2c9a72fe32f66cdbb5de5a680c45cc0ab3902
saikiran_27 <rachakondasaikiran27@gmail.com>
2026-09-21 04:31:37 +0000

    events

--- SC_5/events.cpp
+++ SC_5/events.cpp
@@ -1,29 +1,26 @@
 #include <systemc>
 using namespace sc_core;
 SC_MODULE(events){
-	sc_event t1_event;
 	sc_event t2_event;
 	SC_CTOR(events){
 		SC_THREAD(thread_one);
-		//sensitive<<t1_event;
-		//dont_initialize();
 		SC_THREAD(thread_two);
-		//sensitive<<t2_event;
-		//dont_initialize();
 	}
 	void thread_one(){
 		while(true){
+			t2_event.notify(1,SC_NS);
+			if(sc_time_stamp()==sc_time(4,SC_NS)){
+				t2_event.cancel();
+				std::cout<<sc_time_stamp() << " t1 : event cancelled."<<std::endl;
+			}
+			wait(2,SC_NS);
 			std::cout<<sc_time_stamp()<< " t1 "<<std::endl;
-			t2_event.notify(2,SC_NS);
-			//t2_event.notify(1,SC_NS);
-			wait(t1_event);
 		}
 	}
 	void thread_two(){
 		while(true){
 			wait(t2_event);
 			std::cout<<sc_time_stamp()<<" t2 "<<std::endl;
-			t1_event.notify(1,SC_NS);
 		}
 	}
 };
@@ -31,4 +28,5 @@ int sc_main(int argc, char* argv[]){
 	events event("event");
 	sc_start(10,SC_NS);	
 	return 0;
+
 }
